76938ecba49f87b3b67ea44724a6d0c990e1150c
Jephtebb <jephteborno@yahoo.com>
2020-12-11 20:57:43 -0500

    grab the value of the input form and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,19 +15,22 @@ function addToDo(event){
     const todoDiv = document.createElement('div');
     todoDiv.classList.add('todo')
     const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     //check mark button
     const completedButton = document.createElement('button');
     completedButton.innerHTML = '<i class = "fas fa-check"></i>';
-    completedButton.classList.add('completedBtn');
+    completedButton.classList.add('completed-btn');
     todoDiv.appendChild(completedButton);
     //check trash button
     const trashBtn = document.createElement('button');
     trashBtn.innerHTML = '<i class = "fas fa-trash"></i>';
-    trashBtn.classList.add('trashBtn');
+    trashBtn.classList.add('trash-btn');
     todoDiv.appendChild(trashBtn);
     //append to list
     todoList.appendChild(todoDiv);
+    //clear to do value
+    todoInput.value = '';
 
 }
